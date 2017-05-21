eeafd82f0a9e3b78c8c1bd2a3f646b2f91a7f86a
patonewski <patonewski@o2.pl>
2017-05-22 01:21:06 +0200

    Widoki
    Utworzenie widoków, wyświetlania produktów, CRUD, dodawanie do koszyka,
    debugowanie

---
+++ MVC/MVC/Migrations/201705212233394_InitMigration.cs
@@ -0,0 +1,20 @@
+namespace MVC.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitMigration : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Products", "Category", c => c.String());
+            AddColumn("dbo.Products", "Quantity", c => c.Int(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Products", "Quantity");
+            DropColumn("dbo.Products", "Category");
+        }
+    }
+}
--- MVC/MVC/Models/Product.cs
+++ MVC/MVC/Models/Product.cs
@@ -4,19 +4,27 @@ using System.Data.Entity;
 using System.Linq;
 using System.Web;
 
-namespace MVC
+namespace MVC.Models
 {
     public class Product
     {
         public int Id { set; get; }
         public string ProductName { set; get; }
-        public string Description { set; get; }
+        public string Category { get; set; }
+        public int Quantity { get; set; }
         public float Price { set; get; }
+        public string Description { set; get; }
+        
+
     }
 
 
-    public class ProductDBContext
+    public class ProductDBContext : DbContext
     {
         public DbSet<Product> Products { get; set;}
+    }
+    public class BasketDBContext : DbContext
+    {
+        public DbSet<Basket> Baskets { get; set; }
     }
 }
