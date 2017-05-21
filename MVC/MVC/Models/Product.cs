5ba670b1e78e96feac0846f9893bd7330dc0aa4e
patonewski <patonewski@o2.pl>
2017-05-21 22:50:36 +0200

    CodeBehind
    Dodanie Entity Framework utworzenie klas Product i Koszyk, dodanie
    widoków, modyfikacja połączneia z DB

---
+++ MVC/MVC/Models/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MVC
+{
+    public class Product
+    {
+        public int Id { set; get; }
+        public string ProductName { set; get; }
+        public string Description { set; get; }
+        public float Price { set; get; }
+    }
+
+
+    public class ProductDBContext
+    {
+        public DbSet<Product> Products { get; set;}
+    }
+}
