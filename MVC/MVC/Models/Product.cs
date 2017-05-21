8ce07bd44260220078825f752f0e290b98c05c0a
patonewski <patonewski@o2.pl>
2017-05-22 01:25:07 +0200

    Revert "Widoki"
    This reverts commit eeafd82f0a9e3b78c8c1bd2a3f646b2f91a7f86a.

--- MVC/MVC/Models/Product.cs
+++ MVC/MVC/Models/Product.cs
@@ -4,27 +4,19 @@ using System.Data.Entity;
 using System.Linq;
 using System.Web;
 
-namespace MVC.Models
+namespace MVC
 {
     public class Product
     {
         public int Id { set; get; }
         public string ProductName { set; get; }
-        public string Category { get; set; }
-        public int Quantity { get; set; }
-        public float Price { set; get; }
         public string Description { set; get; }
-        
-
+        public float Price { set; get; }
     }
 
 
-    public class ProductDBContext : DbContext
+    public class ProductDBContext
     {
         public DbSet<Product> Products { get; set;}
-    }
-    public class BasketDBContext : DbContext
-    {
-        public DbSet<Basket> Baskets { get; set; }
     }
 }
